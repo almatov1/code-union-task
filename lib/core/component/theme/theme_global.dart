082956a3faa5483ee27fc23b2332846084d766ef
almatov1 <almatovalshin@gmail.com>
2023-09-07 17:14:46 +0500

    updated config

--- lib/core/component/theme/theme_global.dart
+++ lib/core/component/theme/theme_global.dart
@@ -1,41 +1,41 @@
 import 'package:code_union_task/core/component/theme/colors.dart';
+import 'package:code_union_task/core/config/shared.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class ThemeGlobal {
   static final ThemeData _themeData = ThemeData(
     scaffoldBackgroundColor: StandartColors.approxSolitude,
     textTheme: TextTheme(
-      headlineLarge: GoogleFonts.manrope().copyWith(
+      headlineLarge: manrope.copyWith(
         fontWeight: FontWeight.w600,
         color: StandartColors.black,
         fontSize: 24.0,
         height: 1.6,
       ),
-      titleMedium: GoogleFonts.manrope().copyWith(
+      titleMedium: manrope.copyWith(
         fontWeight: FontWeight.w500,
         color: StandartColors.black,
         fontSize: 15.0,
         height: 1.3,
       ),
-      bodyMedium: GoogleFonts.manrope().copyWith(
+      bodyMedium: manrope.copyWith(
         fontWeight: FontWeight.w400,
         color: StandartColors.black,
         fontSize: 16.0,
         height: 1.5,
       ),
-      labelMedium: GoogleFonts.manrope().copyWith(
+      labelMedium: manrope.copyWith(
         fontWeight: FontWeight.w700,
         color: StandartColors.white,
         fontSize: 16.0,
         height: 1.5,
       ),
-      labelSmall: GoogleFonts.manrope().copyWith(
+      labelSmall: manrope.copyWith(
         fontWeight: FontWeight.w400,
         fontSize: 10.0,
         height: 1.4,
       ),
-      bodySmall: GoogleFonts.manrope().copyWith(
+      bodySmall: manrope.copyWith(
         fontWeight: FontWeight.w400,
         color: StandartColors.amaranth,
         fontSize: 16.0,
--- lib/core/config/shared.dart
+++ lib/core/config/shared.dart
@@ -1,4 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//core
 String applicationName = 'Code Union Task';
 
+//api
 String backendUrl = 'http://45.10.110.181:8080';
 String apiUrl = 'api/v1';
+
+//font
+TextStyle manrope = GoogleFonts.manrope();
